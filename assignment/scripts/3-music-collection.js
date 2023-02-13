0c23be0ae0f4c9278108db8da421ce6fe87d78e5
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-13 16:01:45 -0600

    created addToCollection function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,9 @@
 console.log('***** Music Collection *****')
 let collection =  [];
+
+function addToCollection(title,artist,yearPublished){
+    collection.push([title,artist,yearPublished])
+    console.log('Added the following album to our collection:', title, artist, yearPublished)
+}
+addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971)
+console.log(collection)
