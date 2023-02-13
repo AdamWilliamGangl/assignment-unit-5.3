8b23d4770532dfb33d3e14262fe9c7fe3afe65b9
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-13 17:12:34 -0600

    Added function showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,29 @@
 console.log('***** Music Collection *****')
-let collection =  [];
+let collection = [];
 
-function addToCollection(title,artist,yearPublished){
-    collection.push([title,artist,yearPublished])
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYearPublished: yearPublished
+    }
+    collection.push(album)
     console.log('Added the following album to our collection:', title, artist, yearPublished)
+    return album
 }
-addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971)
-console.log(collection)
+
+function showCollection(array) {
+    console.log('The number of items in our collection is:', array.length);
+    for (item of array) {
+        console.log(item.albumTitle, 'by:', item.albumArtist,'published in:', item.albumYearPublished )
+    }
+}
+
+addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971);
+addToCollection('Light My Fire', 'The Doors', 1967);
+addToCollection('Kashmir', 'Led Zeppelin', 1975);
+addToCollection('Comfortably Numb', 'Pink Floyd', 1980);
+addToCollection('Come Sail Away', 'Styx', 1977);
+addToCollection('Some Nights', 'Fun', 2012);
+
+console.log(showCollection(collection));
