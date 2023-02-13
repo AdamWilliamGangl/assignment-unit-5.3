4fbbe869d5acae9a4473894ef658c885bea5b231
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-13 17:43:00 -0600

    completed findbyartist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,19 +14,22 @@ function addToCollection(title, artist, yearPublished) {
 function showCollection(array) {
     console.log('The number of items in our collection is:', array.length);
     for (item of array) {
-        console.log(item.albumTitle, 'by:', item.albumArtist,'published in:', item.albumYearPublished )
+        console.log(item.albumTitle, 'by:', item.albumArtist, 'published in:', item.albumYearPublished)
     }
 }
 
-function findByArtist(artist){
-let newArray= [];
-for (item of array) {
-    if(item.albumArtist == artist ){
-        console.log(artist, 'was found in the collection!');
-        newArray.push (item)
+function findByArtist(artist) {
+    let newArray = [];
+    for (item of collection) {
+        if (item.albumArtist === artist) {
+            newArray.push(item);
+        }
     }
+    if (newArray.length > 0) {
+        console.log('Your artist was found!'); //Added for testing clarification.
+    }
+    else console.log('Your artist was not found.') //Added for testing clarification.
     console.log(newArray)
-}
 }
 
 console.log(addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971));
@@ -38,4 +41,5 @@ console.log(addToCollection('Some Nights', 'Fun', 2012));
 
 showCollection(collection);
 
-findByArtist('Led Zeppelin')
+findByArtist('Led Zeppelin');
+findByArtist('Backstreet Boys');
