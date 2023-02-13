df68fb4ea18925a8cbc6c2c155a6bd62a1d5e451
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-13 17:44:46 -0600

    syntax and notes.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,14 +9,14 @@ function addToCollection(title, artist, yearPublished) {
     }
     collection.push(album)
     return album
-}
+} //end addToCollection function.
 
 function showCollection(array) {
     console.log('The number of items in our collection is:', array.length);
     for (item of array) {
         console.log(item.albumTitle, 'by:', item.albumArtist, 'published in:', item.albumYearPublished)
     }
-}
+} //end showCollection function.
 
 function findByArtist(artist) {
     let newArray = [];
@@ -26,11 +26,11 @@ function findByArtist(artist) {
         }
     }
     if (newArray.length > 0) {
-        console.log('Your artist was found!'); //Added for testing clarification.
+        console.log('Your artist was found!'); //Added for testing clarification if the artist is found.
     }
-    else console.log('Your artist was not found.') //Added for testing clarification.
+    else console.log('Your artist was not found.') //Added for testing clarification if the artist is not found.
     console.log(newArray)
-}
+} //end findByArtist function.
 
 console.log(addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971));
 console.log(addToCollection('Light My Fire', 'The Doors', 1967));
