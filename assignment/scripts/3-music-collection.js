b6ef767bb57bd0798ff26f5c686b54d3eb57b86f
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-13 17:32:00 -0600

    working on findArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,7 +8,6 @@ function addToCollection(title, artist, yearPublished) {
         albumYearPublished: yearPublished
     }
     collection.push(album)
-    console.log('Added the following album to our collection:', title, artist, yearPublished)
     return album
 }
 
@@ -19,11 +18,24 @@ function showCollection(array) {
     }
 }
 
-addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971);
-addToCollection('Light My Fire', 'The Doors', 1967);
-addToCollection('Kashmir', 'Led Zeppelin', 1975);
-addToCollection('Comfortably Numb', 'Pink Floyd', 1980);
-addToCollection('Come Sail Away', 'Styx', 1977);
-addToCollection('Some Nights', 'Fun', 2012);
+function findByArtist(artist){
+let newArray= [];
+for (item of array) {
+    if(item.albumArtist == artist ){
+        console.log(artist, 'was found in the collection!');
+        newArray.push (item)
+    }
+    console.log(newArray)
+}
+}
 
-console.log(showCollection(collection));
+console.log(addToCollection('Stairway to Heaven', 'Led Zeppelin', 1971));
+console.log(addToCollection('Light My Fire', 'The Doors', 1967));
+console.log(addToCollection('Kashmir', 'Led Zeppelin', 1975));
+console.log(addToCollection('Comfortably Numb', 'Pink Floyd', 1980));
+console.log(addToCollection('Come Sail Away', 'Styx', 1977));
+console.log(addToCollection('Some Nights', 'Fun', 2012));
+
+showCollection(collection);
+
+findByArtist('Led Zeppelin')
