273e93f76c3a70323702cf1593e16956fbf6b0de
Adam Shepard <adam.w.gangl@gmail.com>
2023-02-19 12:47:03 -0600

    stretch goals complete.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,8 +14,9 @@ function addToCollection(title, artist, yearPublished, tracks) {
 
 function showCollection(array) {
     console.log('The number of items in our collection is:', array.length);
-    for (item of array) {
-        console.log(item.albumTitle, 'by:', item.albumArtist, 'published in:', item.albumYearPublished)
+    for (i = 0; i < array.length; i++) {
+        for (x = 0; x < array[i].albumTracks.length; x++)
+            console.log(array[i].albumTitle, 'by:', array[i].albumArtist, 'published in:', array[i].albumYearPublished, array[i].albumTracks[x])
     }
 } //end showCollection function.
 
@@ -33,27 +34,27 @@ function findByArtist(artist) {
     console.log(newArray)
 } //end findByArtist function.
 
-// function search(object) {
-//     let searchArray = [];
-//     for (item of collection) {
-//         for (taco of item.albumTracks){
-//             if (item.albumTracks.name === object.trackName){
-//                 searchArray.push(item);
-//             }
-//         } // end nested for loop
-//         if (item.albumArtist === object.artist && item.albumYearPublished === object.year) { //I read this as searching either an artist & year or a trackname.
-//             searchArray.push(item);
-//         }
-//     } // end primary for loop
-//     if (object.artist == null || object.year == null) {
-//         console.log('No search parameters added');
-//         return 
-//     }
-//     console.log('The following matches were found:');
-//     return searchArray
-// } //end search function.
+function search(object) {
+    let searchArray = [];
+    for (i = 0; i < collection.length; i++) {
+        for (x = 0; x < collection[i].albumTracks.length; x++) {
+            if (collection[i].albumTracks[x].name == object.trackName) {
+                searchArray.push(collection[i]);
+            }
+        } // end nested for loop
+        if (searchArray.length == 00 && collection[i].albumArtist === object.artist && collection[i].albumYearPublished === object.year) { //I read this as searching either an artist & year or a trackname.
+            searchArray.push(collection[i]);
+        }
+    } // end primary for loop
+    if (object.artist == null || object.year == null) {
+        console.log('No search parameters added');
+        return
+    }
+    // console.log('The following matches were found:', searchArray);
+    return searchArray
+} //end search function.
 
-console.log(addToCollection('Led Zeppelin I', 'Led Zeppelin', 1969, [{ name:'Good Times, Bad Times', duration: '2:46' }, { name: 'Dazed and Confused', duration: '6:27' }, { name: 'You Shook Me', duration: '6:28' }]));
+console.log(addToCollection('Led Zeppelin I', 'Led Zeppelin', 1969, [{ name: 'Good Times, Bad Times', duration: '2:46' }, { name: 'Dazed and Confused', duration: '6:27' }, { name: 'You Shook Me', duration: '6:28' }]));
 console.log(addToCollection('L.A. Woman', 'The Doors', 1971, [{ name: 'Dazed and Confused', duration: '6:27' }, { name: 'You Shook Me', duration: '6:28' }]));
 console.log(addToCollection('Led Zeppelin IV', 'Led Zeppelin', 1971, [{ name: 'Dazed and Confused', duration: '6:27' }, { name: 'You Shook Me', duration: '6:28' }]));
 console.log(addToCollection('Dark Side of the Moon', 'Pink Floyd', 1973, [{ name: 'Dazed and Confused', duration: '6:27' }, { name: 'You Shook Me', duration: '6:28' }]));
@@ -69,28 +70,7 @@ console.log('Testing Led Zeppelin- should find a result', search({ artist: 'Led 
 console.log('Testing Backstreet Boys- Should not find a result', search({ artist: 'Backstreet Boys', year: 2001 }));
 console.log('Testing searching nothing- should not find a result', search({}));
 
-function search(object) {
-    let searchArray = [];
-    for (i = 0; i < collection.length; i++) {
-        for (x = 0; x < collection[i].albumTracks.length; x++) {
-            if (collection[i].albumTracks[x].name == object.trackName) {
-                searchArray.push(collection[i]);
-            }
-        } // end nested for loop
-        if (searchArray.length == 00 && collection[i].albumArtist === object.artist && collection[i].albumYearPublished === object.year) { //I read this as searching either an artist & year or a trackname.
-            searchArray.push(collection[i]);
-        }
-    } // end primary for loop
-    if (object.artist == null || object.year == null) {//Removing 84-87 does not solve the problem.
-        console.log('No search parameters added');
-        return
-    }
-    // console.log('The following matches were found:', searchArray);
-    return searchArray //Removing this does not solve the problem.
-} //end search function.
-
-
-console.log('Testing with only track name input matching- should result in a match', search({ artist: 'Backstreet Boys', year: 2005, trackName:'Good Times, Bad Times'})); //This is working.
-console.log('Testing with all inputs not matching should result in no matches', search({ artist: 'Backstreet Boys', year: 2005, trackName: 'Candle in the Wind' })); //This is working.
-console.log('Testing with the atist and year working, but not track name - should result in a match', search({ artist: 'Led Zeppelin', year: 1971, trackName: 'Candle in the Wind' })); //This is working.
-console.log('Testing with no parameters, should result in undefined', search({})); //This is working.
+console.log('Testing with only track name input matching- should result in a match', search({ artist: 'Backstreet Boys', year: 2005, trackName: 'Good Times, Bad Times' }));
+console.log('Testing with all inputs not matching should result in no matches', search({ artist: 'Backstreet Boys', year: 2005, trackName: 'Candle in the Wind' }));
+console.log('Testing with the atist and year working, but not track name - should result in a match', search({ artist: 'Led Zeppelin', year: 1971, trackName: 'Candle in the Wind' }));
+console.log('Testing with no parameters, should result in undefined', search({})); 
